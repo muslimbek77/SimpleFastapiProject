1bd6f2f2c18e3bbce5af2c592858d5c961ece00c
muslimbek77 <muslimjonbekali@mail.ru>
2024-08-01 13:58:08 +0500

    add pydantic modul

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def create_item(item:Item):
     items.append(item)
     return items
 
-@app.get("/items")
+@app.get("/items",response_model=list[Item])
 def list_items(limit:int = 10):
     return items[0:limit]
 
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}",response_model=Item)
 def get_item(item_id:int) -> str:
     if item_id < len(items):
         item = items[item_id]
